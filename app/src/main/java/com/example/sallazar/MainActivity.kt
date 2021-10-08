b7bce2ed79997fbf822cfc466b5fe78c0961389e
Sueda-gul <suedagul.48@gmail.com>
2021-10-08 18:16:25 +0300

    08102021-1816

--- app/src/main/java/com/example/sallazar/MainActivity.kt
+++ app/src/main/java/com/example/sallazar/MainActivity.kt
@@ -2,8 +2,7 @@ package com.example.sallazar
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /** Bu aktivite, kullanıcının bir zar atmasına ve sonucu ekranda görüntülemesine olanak tanır.*/
@@ -11,15 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.button)
-
-        rollButton.setOnClickListener {
-            val toast = Toast.makeText(this, "Tıklandı", Toast.LENGTH_SHORT).show()
-            sallaZari()
-        }
+        rollButton.setOnClickListener { sallaZari() }
     }
-
     /** Zar atılır ve sonucu ekrana günceller.*/
     private fun sallaZari() {
         /** 6 yüzlü zar oluşturulur ve sallanır*/
@@ -27,15 +20,28 @@ class MainActivity : AppCompatActivity() {
         val zar2 = Zar(6).salla()
 
         /** gelen sonuç ekranda güncellenir*/
-        val gelenSonuc1: TextView = findViewById(R.id.textView2)
-        val gelenSonuc2: TextView = findViewById(R.id.textView3)
+        val gelenSonuc1: ImageView = findViewById(R.id.imageView)
+        val gelenSonuc2: ImageView = findViewById(R.id.imageView4)
 
-        gelenSonuc1.text = zar1.toString()
-        gelenSonuc2.text = zar2.toString()
+        when (zar1) {
+            1 -> gelenSonuc1.setImageResource(R.drawable.dice_1)
+            2 -> gelenSonuc1.setImageResource(R.drawable.dice_2)
+            3 -> gelenSonuc1.setImageResource(R.drawable.dice_3)
+            4 -> gelenSonuc1.setImageResource(R.drawable.dice_4)
+            5 -> gelenSonuc1.setImageResource(R.drawable.dice_5)
+            6 -> gelenSonuc1.setImageResource(R.drawable.dice_6)
+        }
+        when (zar2) {
+            1 -> gelenSonuc2.setImageResource(R.drawable.dice_1)
+            2 -> gelenSonuc2.setImageResource(R.drawable.dice_2)
+            3 -> gelenSonuc2.setImageResource(R.drawable.dice_3)
+            4 -> gelenSonuc2.setImageResource(R.drawable.dice_4)
+            5 -> gelenSonuc2.setImageResource(R.drawable.dice_5)
+            6 -> gelenSonuc2.setImageResource(R.drawable.dice_6)
+        }
 
     }
 }
-
 /** Zar sınıfında [1-yüzsayısı] aralığında rassal sayı üretilir.*/
 class Zar(private val yuzSayisi: Int) {
     fun salla(): Int {
